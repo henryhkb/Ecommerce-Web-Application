69aa54faaae3abe9ce42f5220629736cd04b9131
Henry <brandohhenry@gmail.com>
2023-03-09 10:38:13 +0000

    an ecommerce website

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User; 
+//Import the user model class
+use Illuminate\Http\Request;
+//Import the auth Facade
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    //function for redirect
+    public function redirect()
+    {
+        //when the user is trying to login check the user type
+        $usertype = Auth::user()->usertype;
+
+        if($usertype == '1'){
+
+            return view('admin.home');
+        
+        }
+
+        else{
+        
+            return view('dashboard');
+        
+        }
+    }
+}
