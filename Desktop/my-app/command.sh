84bed39a415844c647a81cef0cdae8597e263759
henry <brandohhenry@gmail.com>
2023-08-04 19:26:39 +0000

    Commit, then push the code

---
+++ Desktop/my-app/command.sh
@@ -0,0 +1,2 @@
+gh repo create my-app --public
+git checkout -b update_logo
---
+++ Desktop/my-app/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src=" https://www.propelleraero.com/wp-content/uploads/2021/05/Vector.svg" className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className="App-link"  href=" https://www.propelleraero.com/dirtmate/" target="_blank" rel="noopener noreferrer"> Learn React </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
